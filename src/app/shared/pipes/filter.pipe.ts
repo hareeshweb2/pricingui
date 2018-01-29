c58a3b9e3d699ac28a827029bbd49b17fcc1afa5
Hareesh <hareesh.nalla@accionlabs.com>
2018-01-29 17:43:43 +0530

    filter

--- src/app/shared/pipes/filter.pipe.ts
+++ src/app/shared/pipes/filter.pipe.ts
@@ -1,16 +1,34 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-    name: 'myfilter',
+    name: 'filter',
     pure: false
 })
 export class FilterPipe implements PipeTransform {
     transform(items: any[], filter: string): any {
+        debugger;
         if (!items || !filter) {
             return items;
         }
         // filter items array, items which match and return true will be
         // kept, false will be filtered out
-        return items.filter(item => item.title.indexOf(filter) !== -1);
+        return items.filter(item => item.networkId.indexOf(filter) !== -1);
+    }
+}
+
+
+@Pipe({
+    name: "filterForRates",
+    pure: false
+})
+export class FilterPipe2 implements PipeTransform {
+    transform(items: any[], filter: string): any {
+        debugger;
+        if (!items || !filter) {
+            return items;
+        }
+        // filter items array, items which match and return true will be
+        // kept, false will be filtered out
+        return items.filter(item => item.network.indexOf(filter) !== -1);
     }
 }
--- src/app/shared/pipes/shared-pipes.module.ts
+++ src/app/shared/pipes/shared-pipes.module.ts
@@ -1,11 +1,10 @@
 import { FilterPipe } from './filter.pipe';
+import { FilterPipe2 } from "./filter.pipe";
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
-    imports: [
-        CommonModule
-    ],
-    declarations: [FilterPipe]
+    imports: [CommonModule],
+    declarations: [FilterPipe, FilterPipe2]
 })
-export class SharedPipesModule { }
+export class SharedPipesModule {}
